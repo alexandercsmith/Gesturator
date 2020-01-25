ba3b1c0c82c87a73a6ecbce3f82f53a2ef6b3745
Alexander Smith <acsmith.dev@gmail.com>
2020-01-25 13:33:04 -0500

    add magification gesture

--- Gesturator/Card.swift
+++ Gesturator/Card.swift
@@ -7,12 +7,23 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct Card: View {
   let tapped: Bool
+  @State private var scale: CGFloat = 1.0
   
   var body: some View {
     VStack {
+      Image("yoda")
+        .resizable()
+        .scaleEffect(self.scale)
+        .frame(width: 250, height: 250)
+        .gesture(MagnificationGesture()
+          .onChanged { value in
+            self.scale = value.magnitude
+          }
+        )
       Text("CARD")
         .font(.largeTitle)
         .foregroundColor(Color.white)
